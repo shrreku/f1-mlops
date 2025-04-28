a87538ad75984f7e486b03ec95f9a41e07370851
Shreyash Kumar <kshreyash12345@gmail.com>
2025-04-28 18:17:56 +0530

    Update requirements and Kaggle training script

--- notebooks/train_on_kaggle.py
+++ notebooks/train_on_kaggle.py
@@ -17,6 +17,12 @@ Steps in a Kaggle notebook:
    ```
 """
 import argparse
+import sys
+import os
+
+# Add project root to the Python path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
 from src.train import main
 
 
